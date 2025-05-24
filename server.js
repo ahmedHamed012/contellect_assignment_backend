6ba315483a23e3bb7061fa1dc966c83fb15a0044
ahmedHamed012 <ahmedcina60@yahoo.com>
2025-05-24 16:26:37 +0300

    feat: implement real-time contact locking and unlocking functionality with socket.io

--- server.js
+++ server.js
@@ -5,6 +5,43 @@ require("dotenv").config();
 require("./src/database/db_connect");
 // Create HTTP server
 const http = require("http").createServer(app);
+const socketIO = require("socket.io");
+const io = socketIO(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+let lockedContacts = {};
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  // user locks a contact
+  socket.on("lockContact", (contactId) => {
+    lockedContacts[contactId] = socket.id;
+    io.emit("contactLocked", { contactId, by: socket.id });
+  });
+
+  // user unlocks a contact
+  socket.on("unlockContact", (contactId) => {
+    if (lockedContacts[contactId] === socket.id) {
+      delete lockedContacts[contactId];
+      io.emit("contactUnlocked", { contactId });
+    }
+  });
+
+  // On disconnect, unlock all contacts locked by that user
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+    for (const contactId in lockedContacts) {
+      if (lockedContacts[contactId] === socket.id) {
+        delete lockedContacts[contactId];
+        io.emit("contactUnlocked", contactId);
+      }
+    }
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 
